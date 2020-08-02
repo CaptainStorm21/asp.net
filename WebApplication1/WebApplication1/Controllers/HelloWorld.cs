de7e81f2ae5fc2fd2f2516da1dca82a8b7a0c0e2
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-01 23:25:34 -0700

    New project with new controller

---
+++ WebApplication1/WebApplication1/Controllers/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MovieMVC.Controllers
+{
+    public  class HelloWorldController : Controller
+    {
+        //Get : /HelloWorld/
+        public string Index()
+        {
+            return "this is my default Action";
+        }
+
+        //GET: /HelloWorld/Welcome
+
+        public string Welcome()
+        {
+            return "this is welcome tag";
+        }
+    }
+}
