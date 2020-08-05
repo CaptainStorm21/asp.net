d4e29264a57db7b0d0e3059651a40797ede42b39
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-08-05 01:36:58 -0700

    for loop

---
+++ working_with_models/working_with_models/Controllers/WelcomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.JSInterop;
+using working_with_models.Models;
+
+namespace working_with_models.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Mockdata
+            MockData myData = new MockData();
+            string name = myData.GetName();
+            int numTimes = myData.GetnumTimes();
+
+            ViewData  ["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ working_with_models/working_with_models/Models/MockData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace working_with_models.Models
+{
+    public class MockData
+    {
+        internal string GetName()
+        {
+            return "C# Developer";
+        }
+
+        internal int GetnumTimes()
+        {
+            return 4;
+        }
+    }
+}
